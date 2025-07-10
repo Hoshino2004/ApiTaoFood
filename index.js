75f0d6324a3e2c72e8529e3bb395e25dcbf58490
Nguyen Thinh <thinh631631@gmail.com>
2025-07-10 14:25:42 +0700

    Commit lan cuoi

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-config.json'); // file JSON bạn tải từ Firebase
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://tousehao-default-rtdb.firebaseio.com/"
+  credential: admin.credential.cert({
+    type: process.env.FB_TYPE,
+    project_id: process.env.FB_PROJECT_ID,
+    private_key_id: process.env.FB_PRIVATE_KEY_ID,
+    private_key: process.env.FB_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FB_CLIENT_EMAIL,
+    client_id: process.env.FB_CLIENT_ID,
+    auth_uri: process.env.FB_AUTH_URI,
+    token_uri: process.env.FB_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FB_AUTH_PROVIDER,
+    client_x509_cert_url: process.env.FB_CLIENT_CERT
+  }),
+  databaseURL: process.env.FB_DATABASE_URL
 });
+
 
 const db = admin.database();
 const app = express();
